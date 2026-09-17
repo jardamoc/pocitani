9068dfc7a2cca00ad218a082a605b89af5f407c2
Jaroslav Moc <jarda.moc@gmail.com>
2026-09-17 17:12:37 +0200

    Oznacit nasazeni 2026-09-17 (4.)

--- public/js/version.js
+++ public/js/version.js
@@ -5,7 +5,7 @@
  * Zavislosti: cista data, nic neimportuje -> export-ui.js
  */
 
-export const VERZE = { datum: '2026-09-17', poradi: 3 };
+export const VERZE = { datum: '2026-09-17', poradi: 4 };
 
 /* "2026-09-17" -> "17. 9. 2026". Bez Intl, at je to jiste ceske i na
    zarizeni nastavenem na jiny jazyk. */
